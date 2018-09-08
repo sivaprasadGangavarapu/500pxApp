e2535d7c4bb02f5daaaf94d27a7fb352469cb71c
brn.developers <brn.developers@achiever-7.local>
2018-09-08 18:11:49 +0530

    Initial Commit

---
+++ 500pxApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  500pxApp
+//
+//  Created by brn.developers on 9/8/18.
+//  Copyright © 2018 brn.developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
